df72b0120fa873e4922195fce03245988a6ad69e
yourim01 <162237840+yourim01@users.noreply.github.com>
2024-11-04 12:11:56 +0900

    1_구조체를 가리키는 포인터

---
+++ 9_1.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+
+#define SIZE 3
+
+struct student {
+	int number;
+	char name[20];
+	double grade;
+};
+
+int main(void)
+{
+	struct student s = { 24, "Kim", 4.3 };
+	struct student* p;
+
+	p = &s;
+
+	printf("학번=%d, 이름=%s,  학점=%f \n", s.number, s.name, s.grade);
+	printf("학번=%d, 이름=%s,  학점=%f \n", (*p).number, (*p).name, (*p).grade);
+
+	return 0;
+}
