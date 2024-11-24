73980343a4f8a0ef2a749ecf28819872ac07b4c3
yourim01 <162237840+yourim01@users.noreply.github.com>
2024-11-24 12:45:32 +0900

    은행 계좌 구현하기

---
+++ 12-8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// amount 양수이면 입금, 음수이면 출금
+void save(int amount)
+{
+    static long balance = 0;
+
+    if (amount >= 0)
+        printf("%d \t\t", amount);
+    else
+        printf("\t %d \t", -amount);
+
+    balance += amount;
+    printf("%d \n", balance);
+}
+
+int main(void) {
+    printf("==============================\n");
+    printf("입금 \t출금\t 잔고\n");
+    printf("==============================\n");
+    save(10000);
+    save(50000);
+    save(-10000);
+    save(30000);
+    printf("==============================\n");
+    return 0;
+}
